9b3579f8dd0d09812263b602c17a75edb369551e
MclaughlinSteve <mclaughlinsteve90@gmail.com>
2014-04-12 16:49:16 -0700

    Added GUI files. Ill work on it this week.
    I think we're off next friday for good friday, so we may end up having
    another week to work on this project.

---
+++ SE_ATM_Prototype/SE_ATM_Prototype/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SE_ATM_Prototype
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
---
+++ SE_ATM_Prototype/SE_ATM_Prototype/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SE_ATM_Prototype
+{
+	/// <summary>
+	/// Interaction logic for mainWindow.xaml
+	/// </summary>
+	public partial class mainWindow : Window
+	{
+		public mainWindow()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+		
+		private void test_Method(object sender, RoutedEventArgs e)
+		{
+			WelcomeScreen ws = new WelcomeScreen();
+			ws.Show();
+			this.Close();
+		}
+	}
+}
